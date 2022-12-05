bfad1627866c740aeb965ca325bc7752342a2883
Piotr.Kuchta <piotr.kuchta@dynatrace.com>
2022-12-05 08:56:27 +0100

    2022 day05

---
+++ 2022/day05/taskB.ts
@@ -0,0 +1,52 @@
+import { readFileSync } from 'fs';
+import { join } from "lodash";
+
+const input = readFileSync('input.txt', 'utf-8');
+
+
+const inputObj = parseInput(input.split('\n'));
+
+inputObj.moves.forEach(({move, from, to}) => {
+  let tmp = [];
+
+  for (let i = 0; i < move; i++) {
+    let pop = inputObj.stackz[from - 1].pop();
+    tmp.push(pop!);
+  }
+  for (let i = 0; i < move; i++) {
+    let pop = tmp.pop();
+    inputObj.stackz[to - 1].push(pop!);
+  }
+})
+
+let res = inputObj.stackz.map(st => st[st.length-1]).join('');
+
+console.log(res);
+
+
+function parseInput(inputLines: string[]) {
+
+  let space = inputLines.indexOf('');
+
+  let stackz = [];
+  let moves = [];
+
+  for (let i = 1; i < inputLines[space - 1].length ; i += 4) {
+    let stacc = []
+    for (let j = space - 2; j >= 0 ; j--) {
+      if (inputLines[j][i] !== ' ') {
+        stacc.push(inputLines[j][i]);
+      }
+    }
+    stackz.push(stacc);
+  }
+
+  const regx = /move ([0-9]+) from ([0-9]+) to ([0-9]+)/;
+
+  for (let i = space + 1; i < inputLines.length ; i ++) {
+    let match = inputLines[i].match(regx)!;
+    moves.push({move: Number(match[1]), from: Number(match[2]), to: Number(match[3])});
+  }
+
+  return {stackz, moves};
+}
