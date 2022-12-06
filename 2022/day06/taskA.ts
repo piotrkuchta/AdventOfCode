b329f7a1d767eef834a9540dd0b206f5b0aa1957
Piotr.Kuchta <piotr.kuchta@dynatrace.com>
2022-12-06 17:54:34 +0100

    2022 day06 but actually using brain

--- 2022/day06/taskA.ts
+++ 2022/day06/taskA.ts
@@ -1,14 +1,12 @@
 import { readFileSync } from 'fs';
-import { isEqual } from "lodash";
 
 const input = readFileSync('input.txt', 'utf-8');
 
-
-for (let i = 3; i < input.length; i++) {
-  if (input[i] !== input[i - 1] && input[i] !== input[i - 2] && input[i] !== input[i - 3] &&
-    input[i - 1] !== input[i - 2] && input[i - 1] !== input[i - 3] &&
-    input[i - 2] !== input[i - 3]) {
-    console.log(i + 1);
+const number = 3;
+for (let i = number; i < input.length; i++) {
+  let set = new Set(input.slice(i - number, i));
+  if (set.size === number) {
+    console.log(i);
     break;
   }
 }
--- 2022/day06/taskB.ts
+++ 2022/day06/taskB.ts
@@ -2,28 +2,11 @@ import { readFileSync } from 'fs';
 
 const input = readFileSync('input.txt', 'utf-8');
 
-
-function lookup(i: number) {
-  let found = false;
-  for (let j = 0; j < 14; j++) {
-    for (let k = j + 1; k < 14; k++) {
-      if (input[i - j] === input[i - k]) {
-        console.log(j, k);
-        found = true;
-        break;
-      }
-    }
-    if (found) {
-      break;
-    }
-  }
-  return found;
-}
-
-for (let i = 14; i < input.length; i++) {
-  let found = lookup(i);
-  if (!found) {
-    console.log(i + 1);
+const number = 14;
+for (let i = number; i < input.length; i++) {
+  let set = new Set(input.slice(i - number, i));
+  if (set.size === number) {
+    console.log(i);
     break;
   }
 }
